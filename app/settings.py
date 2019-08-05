fb79d34f95290ec9c60078701dc781f613febce4
狮屎胜于熊便 <pfdiy@hotmail.com>
2019-08-05 09:38:39 +0800

    update debug

--- app/settings.py
+++ app/settings.py
@@ -31,6 +31,7 @@ class DevelopmentConfig(BaseConfig):
 
 
 class ProductionConfig(BaseConfig):
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql://root:root@db/cms'
     CACHE_REDIS_URL = 'redis://user:kml7oQCKE5pXMwyz@redis:6379/0'
 
